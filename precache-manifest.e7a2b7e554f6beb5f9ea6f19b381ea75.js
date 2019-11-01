d305aa752c946107f7e761c576692fd7b7c34332
Arthur <arthurd800@wp.pl>
2019-11-01 17:26:35 +0100

    Updates

---
+++ precache-manifest.e7a2b7e554f6beb5f9ea6f19b381ea75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6074016bf9fb5f8dc8668e565a152a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9fb87fd15ac096fa3a15",
+    "url": "/robofriends/static/css/main.0a148ca8.chunk.css"
+  },
+  {
+    "revision": "9f52f0c4861e117477fc",
+    "url": "/robofriends/static/js/2.a09a1be0.chunk.js"
+  },
+  {
+    "revision": "9fb87fd15ac096fa3a15",
+    "url": "/robofriends/static/js/main.39bfefc0.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
